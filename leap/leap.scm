40e4bc11cc29e45d85f92b2d5fb05aee3a6e1f80
Aldán Creo <20495460+ACMCMC@users.noreply.github.com>
2021-06-11 01:40:07 +0200

    Update leap.scm

--- leap/leap.scm
+++ leap/leap.scm
@@ -1,4 +1,4 @@
 (import (rnrs))
 
 (define (leap-year? year)
-  (if (= (remainder year 4) 0) (if (= (remainder year 100) 0) (if (= (remainder year 400) 0) #t #f) #t) #f))
+  (if (= (remainder year 4) 0) (if (= (remainder year 100) 0) (if (= (remainder year 400) 0) #t #f) #t) #f)) ; A leap year must (1) be divisible by 4; (2) not be divisible by 100 but (3) not by 400 (if it's divisible by 100 and 400, then it's a leap year)
