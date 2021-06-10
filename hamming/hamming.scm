823feefe51f3114673bd8ded961ef4f7341c4c72
Aldán Creo <20495460+ACMCMC@users.noreply.github.com>
2021-06-11 01:46:13 +0200

    Update hamming.scm

--- hamming/hamming.scm
+++ hamming/hamming.scm
@@ -1,13 +1,14 @@
 (import (rnrs))
 
+; Calculates the Hamming distance (number of differences) between two strings
 (define (hamming-distance strand-a strand-b)
-    (cond ((not (= (string-length strand-a)
+    (cond ((not (= (string-length strand-a) ; If the length of the two strings is not the same...
                    (string-length strand-b)))
-           (error "Different length" "Check"))
-          ((= (string-length strand-a) 0) 0)
-          ((string=? (substring strand-a 0 1)
+           (error "Different length" "Check")) ; ...throw an error
+          ((= (string-length strand-a) 0) 0) ; If the length of the strings is 0, return 0 (stop condition)
+          ((string=? (substring strand-a 0 1) ;  If the first character of the strings is equal...
                      (substring strand-b 0 1))
-           (hamming-distance (substring strand-a 1 (string-length strand-a))
+           (hamming-distance (substring strand-a 1 (string-length strand-a)) ; ...recurse with the rest of the list
                              (substring strand-b 1 (string-length strand-b))))
-          (else (+ 1 (hamming-distance (substring strand-a 1 (string-length strand-a))
-                                       (substring strand-b 1 (string-length strand-b)))))))
+          (else (+ 1 (hamming-distance (substring strand-a 1 (string-length strand-a)) ; If the first character of both strings is not equal...
+                                       (substring strand-b 1 (string-length strand-b))))))) ; ...recurse, but add 1 to the result (there's a difference, so Hamming's value is incremented)
